040b71612283a07d3b74526864ecb8fb6f65cd43
Ricky <ricky.y.fok@gmail.com>
2021-02-05 12:38:48 -0800

    Update

--- Keypoints/ScaleSPGen.py
+++ Keypoints/ScaleSPGen.py
@@ -11,15 +11,39 @@ import matplotlib.pyplot as plt
 
 class OctaveGenerator:
 
-    def __init__(self, img, blurNum):
+    def __init__(self, img, blurNum, sigma):
         self.img = img
+        self.sigma = sigma
+        self.blurNum = blurNum
+        self.blur_ksize = (15,15) 
         self.arrData = np.asarray(self.img)
-        self.space = [0] * blurNum   # 2D array representing octaves with blur levels
+        self.space = [0] * blurNum   # array representing octaves with blur levels
 
     def displayOGImg(self):
         plt.imshow(self.img, cmap='gray')
         plt.show()
 
-    # def createOctaves():
+    def createOctaves(self):
+        initialImg = self.img
+        tempSig = self.sigma
+        for x in range(self.blurNum):
+            if x == 0:
+                print(f"Blur {self.blurNum} Generated")
+                self.space[x] = initialImg
+                x += 1
+            else:
+                tempImg = cv2.GaussianBlur(initialImg, self.blur_ksize, sigmaX=self.sigma*1.6, sigmaY=self.sigma*1.6)
+                self.space[x] = tempImg
+                tempSig = tempSig * (2 ** 0.5)  #sigma differ by factor of root 2
+                x += 1
+
+        for y in range(len(self.space)):
+            k = cv2.waitKey(0) & 0xFF
+            if(k == 32):
+                cv2.imshow(self.space[y])   
+            y += 1
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
 
 
--- Keypoints/main.py
+++ Keypoints/main.py
@@ -12,12 +12,14 @@ from ScaleSPGen import OctaveGenerator
 def main(): # main method
     oNum = 4    # input("How many octave levels: ") #number of octaves
     bNum = 5    # input("How many blur levels: ")   #number of blurs
+    sigNum = 0.707103   #input("Sigma Value: ") #TODO check sigma value
     mainImg = readImg()
-    scaleSpace = OctaveGenerator(mainImg, int(bNum))
-    scaleSpace.displayOGImg()
+    scaleSpace = OctaveGenerator(mainImg, int(bNum), float(sigNum))
+    scaleSpace.createOctaves()
+    #scaleSpace.displayOGImg()
 
 def readImg():  # reads in image
-    img = cv2.imread('cat_image.jpg')
+    img = cv2.imread('cat_image.jpg', cv2.IMREAD_GRAYSCALE)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  #allows for correct matplotlib display since in RGB instead of BGR
     img = cv2.resize(img, (0,0), fx = 2, fy = 2) #doubles image size
     img = cv2.GaussianBlur(img, (3,3), sigmaX=0.707103, sigmaY=0.707103)    #anti-alliasing TODO: check sigma values
